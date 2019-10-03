d0d773e6da134dec9204d4337bcae99af84ab535
claukers <claukers@melia>
2019-10-03 17:11:49 -0300

    added tests for modelhandler

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,2 +1,53 @@
-export * from "./model";
-export * from "./common";
+import { Router } from "express";
+import { Util } from "miqro-core";
+import { IAPIRequest, IServiceHandler, IServiceRouteOptions, ServiceArg, ServiceResponse, ServiceRoute } from "miqro-express";
+import { IModelService } from "miqro-sequelize";
+
+export const createModelHandler = (service: IModelService, logger): IServiceHandler => {
+  const router = Router();
+  // Get All
+  router.get("/", async (req: IAPIRequest, res) => {
+    const ret = await service.get(new ServiceArg(req));
+    logger.debug(`${req.method} handler ret [${ret}]`);
+    await new ServiceResponse(ret).send(res);
+  });
+  // Get by Id
+  router.get("/:id", async (req: IAPIRequest, res) => {
+    const ret = await service.get(new ServiceArg(req));
+    logger.debug(`${req.method} handler ret [${ret}]`);
+    await new ServiceResponse(ret).send(res);
+  });
+  // Post
+  router.post("/", async (req: IAPIRequest, res) => {
+    const ret = await service.post(new ServiceArg(req));
+    logger.debug(`${req.method} handler ret [${ret}]`);
+    await new ServiceResponse(ret).send(res);
+  });
+  // Delete by id
+  router.delete("/:id", async (req: IAPIRequest, res) => {
+    const ret = await service.delete(new ServiceArg(req));
+    logger.debug(`${req.method} handler ret [${ret}]`);
+    await new ServiceResponse(ret).send(res);
+  });
+  // Patch by id
+  router.patch("/:id", async (req: IAPIRequest, res) => {
+    const ret = await service.patch(new ServiceArg(req));
+    logger.debug(`${req.method} handler ret [${ret}]`);
+    await new ServiceResponse(ret).send(res);
+  });
+  // Patch by id
+  router.put("/", async (req: IAPIRequest, res) => {
+    const ret = await service.put(new ServiceArg(req));
+    logger.debug(`${req.method} handler ret [${ret}]`);
+    await new ServiceResponse(ret).send(res);
+  });
+  return router;
+};
+
+export class ModelRoute extends ServiceRoute {
+  constructor(protected service: IModelService, options?: IServiceRouteOptions) {
+    super(options);
+    const logger = Util.getLogger("ModelServiceRoute");
+    this.use(undefined, createModelHandler(this.service, logger));
+  }
+}
---
+++ test/modelhandler.func.test.ts
@@ -0,0 +1,166 @@
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import * as express from "express";
+import * as sinon from "sinon";
+import * as request from "supertest";
+
+
+describe("modelhandler functional tests", () => {
+  it("createModelHandler get all happy path", (done) => {
+    const { createModelHandler } = require("../src/");
+
+    const fakeToken = "FakeToken";
+    const fakeInstance = "FakeInstance";
+    process.env.TOKEN_HEADER = "TOKEN_HEADER";
+
+    const app = express();
+    const modelService = {
+      get: sinon.fake(async (args) => {
+        expect(args.params.id).to.be.equals(undefined);
+        return fakeInstance;
+      }),
+      post: sinon.fake(async (args) => {
+
+      }),
+      patch: sinon.fake(async (args) => {
+
+      }),
+      delete: sinon.fake(async (args) => {
+
+      })
+    };
+    const logger = {
+      debug: (text) => {
+        console.log(text);
+      }
+    }
+    const finalHandler = sinon.fake((req, res) => {
+    });
+    app.use("/user", [createModelHandler(modelService, logger), finalHandler]);
+
+    request(app)
+      .get('/user')
+      .set({ 'TOKEN_HEADER': fakeToken })
+      .expect('Content-Type', /json/)
+      .expect('Content-Length', '40')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        } else {
+          expect(res.body.success).to.be.equals(true);
+          expect(res.body.result).to.be.equals(fakeInstance);
+          expect(finalHandler.callCount).to.be.equals(0);
+          expect(modelService.get.callCount).to.be.equals(1);
+          expect(modelService.post.callCount).to.be.equals(0);
+          expect(modelService.patch.callCount).to.be.equals(0);
+          expect(modelService.delete.callCount).to.be.equals(0);
+          done();
+        }
+      });
+  });
+
+  it("createModelHandler get by id happy path", (done) => {
+    const { createModelHandler } = require("../src/");
+
+    const fakeId = "FakeId";
+    const fakeToken = "FakeToken";
+    const fakeInstance = "FakeInstance";
+    process.env.TOKEN_HEADER = "TOKEN_HEADER";
+
+    const app = express();
+    const modelService = {
+      get: sinon.fake(async (args) => {
+        expect(fakeId).to.be.equals(args.params.id);
+        return fakeInstance;
+      }),
+      post: sinon.fake(async (args) => {
+
+      }),
+      patch: sinon.fake(async (args) => {
+
+      }),
+      delete: sinon.fake(async (args) => {
+
+      })
+    };
+    const logger = {
+      debug: (text) => {
+        console.log(text);
+      }
+    }
+    app.use("/user", [createModelHandler(modelService, logger)]);
+
+    request(app)
+      .get('/user/' + fakeId)
+      .set({ 'TOKEN_HEADER': fakeToken })
+      .expect('Content-Type', /json/)
+      .expect('Content-Length', '40')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        } else {
+          expect(res.body.success).to.be.equals(true);
+          expect(res.body.result).to.be.equals(fakeInstance);
+          expect(modelService.get.callCount).to.be.equals(1);
+          expect(modelService.post.callCount).to.be.equals(0);
+          expect(modelService.patch.callCount).to.be.equals(0);
+          expect(modelService.delete.callCount).to.be.equals(0);
+          done();
+        }
+      });
+  });
+
+  it("createModelHandler patch by id happy path", (done) => {
+    const { createModelHandler } = require("../src/");
+
+    const fakeId = "FakeId";
+    const fakeToken = "FakeToken";
+    const fakeInstance = "FakeInstance";
+    process.env.TOKEN_HEADER = "TOKEN_HEADER";
+
+    const app = express();
+    const modelService = {
+      get: sinon.fake(async (args) => {
+        
+      }),
+      post: sinon.fake(async (args) => {
+
+      }),
+      patch: sinon.fake(async (args) => {
+        expect(fakeId).to.be.equals(args.params.id);
+        return fakeInstance;
+      }),
+      delete: sinon.fake(async (args) => {
+
+      })
+    };
+    const logger = {
+      debug: (text) => {
+        console.log(text);
+      }
+    }
+    app.use("/user", [createModelHandler(modelService, logger)]);
+
+    request(app)
+      .patch('/user/' + fakeId)
+      .set({ 'TOKEN_HEADER': fakeToken })
+      .expect('Content-Type', /json/)
+      .expect('Content-Length', '40')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        } else {
+          expect(res.body.success).to.be.equals(true);
+          expect(res.body.result).to.be.equals(fakeInstance);
+          expect(modelService.get.callCount).to.be.equals(0);
+          expect(modelService.post.callCount).to.be.equals(0);
+          expect(modelService.patch.callCount).to.be.equals(1);
+          expect(modelService.delete.callCount).to.be.equals(0);
+          done();
+        }
+      });
+  });
+});
