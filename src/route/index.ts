696cd9636a8b382a0938467a8a409591d14b5050
claukers <claukers@ameila>
2020-03-12 00:02:55 -0300

    MapModelHandler added

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,7 +1,33 @@
 import {Router} from "express";
 import {MethodNotImplementedError} from "miqro-core";
-import {Handler, ResponseHandler} from "miqro-express";
+import {getResults, Handler, ResponseHandler, setResults} from "miqro-express";
 import {IModelService} from "miqro-sequelize";
+
+export const MapModelHandler = (callbackfn: (value: any, index: number, array: any[]) => any, logger?: any) => {
+  return Handler(async (req) => {
+    const results = getResults(req);
+    if (results) {
+      const mappedResults = results.map((result) => {
+        if (!result) {
+          return null;
+        } else {
+          if (result instanceof Array) {
+            return result.map(callbackfn);
+          } else if (result.rows instanceof Array) {
+            return {
+              count: result.count,
+              rows: result.rows.map(callbackfn)
+            };
+          } else {
+            const [ret] = [result].map(callbackfn);
+            return ret;
+          }
+        }
+      });
+      setResults(req, mappedResults);
+    }
+  }, logger);
+};
 
 export const ModelHandler = (service: IModelService, logger?) => {
   return Handler(async (req) => {
